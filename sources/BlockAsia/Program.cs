6507253fc72aa3b1e6a2de2b60001ef655b3049e
t-kadowaki <t-kadowaki@onecompath.com>
2020-03-19 17:51:01 +0900

    :construction: Add Library for Argument Processing.

--- sources/BlockAsia/Program.cs
+++ sources/BlockAsia/Program.cs
@@ -1,12 +1,41 @@
-﻿using System;
+using System;
+using McMaster.Extensions.CommandLineUtils;
 
 namespace BlockAsia
 {
     class Program
     {
-        static void Main(string[] args)
+        [Option(Description = "Specify the client id for the service account.")]
+        public String ClientId { get; }
+
+        [Option(Description = "Specify the client secret for the service account.")]
+        public String Secret { get; }
+
+        [Option(Description = "Specify the project id for the firewall installation project.")]
+        public String ProjectId { get; }
+
+        [Option(Description = "Specify the IP address list file to block.")]
+        public String FilePath { get; }
+
+        // エントリポイントはコマンドラインパーサーを実行する
+        static int Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+
+        /// <summary>
+        /// メイン処理
+        /// </summary>
+        private void OnExecute()
         {
-            Console.WriteLine("Hello World!");
+            // 処理開始
+            requiredConfirmation();
+            Console.WriteLine("Hello,World");
         }
+
+        /// <summary>
+        /// 引数必須確認
+        /// </summary>
+        private void requiredConfirmation()
+        {
+        }
+
     }
 }
